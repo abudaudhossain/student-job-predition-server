08d0e6e3bb0f981a1482ce07e79e2c5b92fe4488
abudaudhossain <abudaudhossainsumon@gmail.com>
2026-04-02 08:57:24 +0600

    fixed cros issue

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import pandas as pd
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = joblib.load("model.pkl")
 MODEL_FEATURES = list(model.feature_names_in_)
